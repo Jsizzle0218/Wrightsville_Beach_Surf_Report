d8cefd1ba3c662f753ab4085f6e05d6d9695d0a3
Jsizzle0218 <jacslem218@gmail.com>
2026-05-29 21:28:28 -0400

    WaveAppSimp.py
    simplified code

--- WaveApp.py
+++ WaveApp.py
@@ -3,117 +3,41 @@ import streamlit as st
 st.title("Surf Report App")
 
 date = st.text_input("enter date: (Ex: 5/27/2026)")
-time = st.selectbox(
-    "Time of day",
-    ["Morning", "Afternoon", "Evening"]
-)
-tide = st.selectbox(
-    "Tide",
-    ["High", "Low", "Outgoing", "Incoming"]
-)
+time = st.selectbox("Time of day", ["Morning", "Afternoon", "Evening"])
+tide = st.selectbox("Tide", ["High", "Low", "Outgoing", "Incoming"])
 wave_height = st.number_input("Wave height (single rational number only):")
 wind = st.number_input("Wind speed in kts (single rational number only):")
-wind_direction = st.selectbox(
-    "wind direction",
-    ["offshore", "onshore", "cross shore"]
-)
+wind_direction = st.selectbox("wind direction", ["offshore", "onshore", "cross shore"])
 
-
-if wind < 10 and wind_direction == "offshore":
-
-    if wave_height < 2:
-        st.write("Best wind conditions but waves are tiny")
+def wave_rate(wave_height):
     
-    if 2 <= wave_height < 4:
-        st.write("Best wind conditions and the waves are medium sized")
-
-    if wave_height >= 4:
-        st.write("Best wind conditions and the waves are really big")
-
-if wind < 10 and wind_direction == "onshore":
-
     if wave_height < 2:
-        st.write("Wind may create small chop but waves are tiny")
-    
-    if 2 <= wave_height < 4:
-        st.write("Wind may create small chop but the waves are rideable")
+        return "Small waves"
+    elif wave_height < 4:
+        return "Rideable waves"
+    else: 
+        return "Big waves"
 
-    if wave_height >= 4:
-        st.write("Wind may create small chop but the waves are really big")
+def wind_rate(wind, wind_direction):
 
-if wind < 10 and wind_direction == "cross shore":
+    if wind < 10 and wind_direction == "offshore":
+        return "with excellent offshore wind"
+    elif wind < 10 and wind_direction == "onshore":
+        return "with nice onshore wind"
+    elif wind < 10 and wind_direction == "cross shore":
+        return "with nice cross shore wind"
+    elif 10 <= wind < 20 and wind_direction == "onshore":
+        return "with slight choppy onshore wind"
+    elif 10 <= wind < 20 and wind_direction == "cross shore":
+        return "with slight drifty cross shore wind"
+    elif 10 <= wind < 20 and wind_direction == "offshore":
+        return "with nice offshore wind"
+    elif wind >= 20 and wind_direction == "onshore":
+        return "with very choppy onshore wind"
+    elif wind >= 20 and wind_direction == "cross shore":
+        return "with very drifty cross shore wind"
+    elif wind >= 20 and wind_direction == "offshore":
+        return "with strong offshore wind"
 
-    if wave_height < 2:
-        st.write("Possible small north or south current but waves are tiny")
-    
-    if 2 <= wave_height < 4:
-        st.write("Possible small north or south current and rideable waves are likely")
-
-    if wave_height >= 4:
-        st.write("Possible small north or south current and the waves are really big")
-
-if 10 <= wind < 20 and wind_direction == "onshore":
-
-    if wave_height < 2:
-        st.write("Wind will make waves a little choppy but waves are tiny")
-    
-    if 2 <= wave_height < 4:
-        st.write("Wind will make waves a little choppy but waves are rideable")
-
-    if wave_height >= 4:
-        st.write("Wind will make waves a little choppy but waves are really big")
-
-if 10 <= wind < 20 and wind_direction == "cross shore":
-
-    if wave_height < 2:
-        st.write("Some current going north or south and the waves are tiny")
-    
-    if 2 <= wave_height < 4:
-        st.write("Some current going north or south but waves could be surfable")
-
-    if wave_height >= 4:
-        st.write("Some current going north or south but the waves are really big")
-
-if 10 <= wind < 20 and wind_direction == "offshore":
-
-    if wave_height < 2:
-        st.write("Wind is nice but the waves are tiny")
-    
-    if 2 <= wave_height < 4:
-        st.write("Wind is nice and likely to catch some medium waves")
-
-    if wave_height >= 4:
-        st.write("Wind is nice and waves are really big")
-
-if wind >= 20 and wind_direction == "onshore":
-
-    if wave_height < 2:
-        st.write("Strong wind and possible current, choppy and small waves")
-    
-    if 2 <= wave_height < 4:
-        st.write("Strong wind and possible current, choppy but rideable waves possible")
-
-    if wave_height >= 4:
-        st.write("Strong wind and possible current, choppy but really big waves")
-
-if wind >= 20 and wind_direction == "cross shore":
-
-    if wave_height < 2:
-        st.write("Strong current going north or south, and the waves are small")
-    
-    if 2 <= wave_height < 4:
-        st.write("Strong current going north or south, but waves could be rideable")
-
-    if wave_height >= 4:
-        st.write("Strong current going north or south, but the waves are really big")
-
-if wind >= 20 and wind_direction == "offshore":
-
-    if wave_height < 2:
-        st.write("Difficult to paddle when catching a wave, and the waves are small")
-    
-    if 2 <= wave_height < 4:
-        st.write("Difficult to paddle when catching a wave, but rideable waves are likely")
-
-    if wave_height >= 4:
-        st.write("Difficult to paddle when catching a wave, but the waves are really big")
+if st.button("Submit"):
+    st.write(wave_rate(wave_height) + " " + wind_rate(wind, wind_direction))
